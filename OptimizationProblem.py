c73d1351244e57770ffceab53975d3bf5ffc30a3
Beste Aydemir <besteaydemir99@gmail.com>
2021-05-13 22:31:45 +0300

    g

--- OptimizationProblem.py
+++ OptimizationProblem.py
@@ -1,18 +1,22 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class OptimizationProblem:
-    def __init__(self, func_list = None, dataset = None, scaler=None):
+    def __init__(self, cardinality, N, D_1, func_list = None, dataset = None, scaler=None):
         self.func_list = func_list
         if func_list is not None:
             self.m = len(func_list)
         self.m = 2
         if scaler is not None:
             self.scaler =scaler
-        self.cardinality = 2500
-        self.N = 2
-        self.D_1 = 2
+        self.cardinality = cardinality
+        self.N = N
+        self.D_1 = D_1
         self.alpha = 1
+
+        # Observed so far
+        self.xlist=[]
+        self.ylist=[]
 
         if dataset is not None:
             self.x = dataset[0]
@@ -41,6 +45,41 @@ class OptimizationProblem:
             #print(obs)
 
         return obs
+
+    def plot_gp_1d(self, rang, gp, x, y):
+        xx = np.linspace(rang[0], rang[1], 100).reshape(100, 1)
+
+        ## predict mean and variance of latent GP at test points
+        mean, var = gp.model[0].predict_f(xx)
+
+        fig, ax = plt.subplots(figsize=(12, 8))
+
+        # Points
+        if self.xlist:
+            ax.plot(self.xlist, self.ylist, "kx", mew=2)
+
+
+        ax.plot(x[0][0], y[:,0][0], "kx", mew=2)
+
+        self.xlist.append(float(x))
+        self.ylist.append(float(y[:,0]))
+
+        # Func
+        ax.plot(xx, self.func_list[0](xx))
+
+        # GP
+        ax.plot(xx, mean, "C6", lw=2)
+        ax.fill_between(
+            xx[:, 0],
+            mean[:, 0] - 1.96 * np.sqrt(var[:, 0]),
+            mean[:, 0] + 1.96 * np.sqrt(var[:, 0]),
+            color="C0",
+            alpha=0.2,
+        )
+
+        plt.xlim([0, 1])
+        plt.ylim([-1.5, 1.5])
+        plt.show()
 
     # def observe(self, x):
     # find the closest
